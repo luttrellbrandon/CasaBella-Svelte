054110f2cab7a59954ca3e2470c2b4339fe94c2f
Maximus <brandon.luttrell@keenahealth.com>
2026-03-09 21:45:32 -0400

    more changes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
   import { site } from '$lib/content/site';
 </script>
 
-<footer class="footer footer-center bg-base-200 text-base-content p-6 mt-auto">
+<footer class="footer footer-center p-6 mt-auto text-base-content/90">
   <aside>
     <p>{site.copyright}</p>
   </aside>
