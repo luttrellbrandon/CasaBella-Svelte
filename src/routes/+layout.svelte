626570fcb4a4a19ec82eacad8dbc5014bfd5d9c3
Maximus <brandon.luttrell@keenahealth.com>
2026-03-09 21:58:40 -0400

    minor changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,8 @@
   import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<div class="site-background" aria-hidden="true"></div>
-<div class="flex min-h-screen flex-col relative">
+<div class="app-background" aria-hidden="true"></div>
+<div class="relative z-[1] flex min-h-screen flex-col">
   <slot />
   <Footer />
 </div>
